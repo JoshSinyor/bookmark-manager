e06bd96abe013979651e7805abf2410850fc04dd
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-29 17:53:19 +0000

    Updated bookmark_spec.rb. This reflects error correction.

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,9 +7,9 @@ describe Bookmark do
 
   describe '.all' do
     it 'returns all bookmarks' do
-      con.exec("INSERT INTO bookmarks (url, title) VALUES('#{BBC_NEWS_URL}', '#{BBC_NEWS_TITLE}')")
-      con.exec("INSERT INTO bookmarks (url, title) VALUES('#{GOOGLE_URL}', '#{GOOGLE_TITLE}')")
-      con.exec("INSERT INTO bookmarks (url, title) VALUES('#{MAKERS_URL}', '#{MAKERS_TITLE}')")
+      con.exec "INSERT INTO bookmarks (url, title) VALUES('#{BBC_NEWS_URL}', '#{BBC_NEWS_TITLE}')"
+      con.exec "INSERT INTO bookmarks (url, title) VALUES('#{GOOGLE_URL}', '#{GOOGLE_TITLE}')"
+      con.exec "INSERT INTO bookmarks (url, title) VALUES('#{MAKERS_URL}', '#{MAKERS_TITLE}')"
 
       expect(Bookmark.all[0].title).to include(BBC_NEWS_TITLE)
       expect(Bookmark.all[1].title).to include(GOOGLE_TITLE)
@@ -29,27 +29,27 @@ describe Bookmark do
   describe '.delete' do
     it 'deletes a specific bookmark from the database' do
       con.exec "INSERT INTO bookmarks (url, title) VALUES('#{BBC_NEWS_URL}', '#{BBC_NEWS_TITLE}')"
-      rs = con.exec "INSERT INTO bookmarks (url, title) VALUES('#{GOOGLE_URL}', '#{GOOGLE_TITLE}') RETURNING *"
+      rs = con.exec("INSERT INTO bookmarks (url, title) VALUES('#{GOOGLE_URL}', '#{GOOGLE_TITLE}') RETURNING *")[0]
       con.exec "INSERT INTO bookmarks (url, title) VALUES('#{MAKERS_URL}', '#{MAKERS_TITLE}')"
 
-      Bookmark.delete(rs[0]['id'])
+      Bookmark.delete(rs['id'])
 
-      rs = con.query "SELECT EXISTS (SELECT title FROM bookmarks WHERE id='#{rs[0]['id']}')"
-      expect(rs[0]['exists']).to eq 'f'
+      rs = con.query("SELECT EXISTS (SELECT title FROM bookmarks WHERE id='#{rs['id']}')")[0]
+      expect(rs['exists']).to eq 'f'
     end
   end
 
   describe '.update' do
     it 'updates a bookmark from the database' do
       con.exec "INSERT INTO bookmarks (url, title) VALUES('#{BBC_NEWS_URL}', '#{BBC_NEWS_TITLE}') RETURNING *"
-      rs = con.exec "INSERT INTO bookmarks (url, title) VALUES('#{GOOGLE_URL}', '#{GOOGLE_TITLE}') RETURNING *"
+      rs = con.exec("INSERT INTO bookmarks (url, title) VALUES('#{GOOGLE_URL}', '#{GOOGLE_TITLE}') RETURNING *")[0]
       con.exec "INSERT INTO bookmarks (url, title) VALUES('#{MAKERS_URL}', '#{MAKERS_TITLE}') RETURNING *"
 
       Bookmark.update(rs[0]['id'], HACKER_TYPER_URL, HACKER_TYPER_TITLE)
 
-      rs = con.query "SELECT * FROM bookmarks WHERE id='#{rs['id']}')"
-      expect(rs[0]['url']).to eq HACKER_TYPER_URL
-      expect(rs[0]['title']).to eq HACKER_TYPER_TITLE
+      rs = con.query("SELECT * FROM bookmarks WHERE id='#{rs['id']}')")[0]
+      expect(rs['url']).to eq HACKER_TYPER_URL
+      expect(rs['title']).to eq HACKER_TYPER_TITLE
     end
   end
 end
