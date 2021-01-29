7dfc519b153f28eb12690dba2df0f4facdde3289
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-29 16:26:07 +0000

    Updated deleting_bookmark_spec.rb and index.erb. This reflects adding class and id to the Bookmark instances on index.erb.

--- spec/features/deleting_bookmark_spec.rb
+++ spec/features/deleting_bookmark_spec.rb
@@ -4,6 +4,8 @@ feature 'Deleting a bookmark' do
   scenario 'user adds three bookmarks and deletes the second' do
     add_bookmark_feature(SITE_BBC_NEWS_URL, SITE_BBC_NEWS_TITLE)
     click_button('Delete Bookmark')
+# Got to get ID for this to work.
     expect(page).not_to have_content(SITE_BBC_NEWS_TITLE)
+# Expect it not to have link as well.
   end
 end
